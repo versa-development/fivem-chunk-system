dd7baf9b1994b3dec093e7e869b1a078c070bb85
Aidan <44532573+ohaidan@users.noreply.github.com>
2025-05-25 00:43:12 +0100

    delete old server files

--- server/sv_exports.lua
+++
@@ -1,7 +0,0 @@
-exports('CreateObject', function(model, coords, targetData)
-  -- TODO: Implement object creation logic
-end)
-
-exports('CreatePed', function(model, coords, targetData)
-  -- TODO: Implement ped creation logic
-end)
--- server/sv_init.lua
+++
@@ -1,9 +0,0 @@
-Chunk = {}
-Chunk.__index = Chunk
-
-Entities = {}
-Entities.__index = Entities
-
-CreateThread(function()
-  -- TODO: Initalise Chunk zones here
-end)
