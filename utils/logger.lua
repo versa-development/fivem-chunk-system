ad6f3b3f7cdb5ea9d97e87990cfaaed54fd2ca5a
Aidan <44532573+ohaidan@users.noreply.github.com>
2025-05-25 16:04:46 +0100

    refactor(utils/logger) support multiple params

--- utils/logger.lua
+++ utils/logger.lua
@@ -1,11 +1,32 @@
-local logger = {}
+local config = require 'data.config'
 
-function logger.info(msg)
-  print('[Versa Chunk System] ' .. msg)
+local Logger = {}
+
+--- Concatenates arguments into a single string, converting each argument to a string.
+local function formatLogMessage(...)
+  local args = {...}
+  for i = 1, #args do
+    args[i] = tostring(args[i])
+  end
+  return table.concat(args, ' ')
 end
 
-function logger.error(msg)
-  print('[Versa Chunk System] [ERROR] ' .. msg)
+function Logger.info(...)
+  print('^5[Versa Chunks] ^7' .. formatLogMessage(...))
 end
 
-return logger
+function Logger.warn(...)
+  print('^5[Versa Chunks] ^3[WARNING] ' .. formatLogMessage(...) .. '^7')
+end
+
+function Logger.error(...)
+  print('^5[Versa Chunks] ^1[ERROR] ' .. formatLogMessage(...) .. '^7')
+end
+
+function Logger.debug(...)
+  if config.Debug then
+    print('^5[Versa Chunks] ^2[DEBUG] ' .. formatLogMessage(...) .. '^7')
+  end
+end
+
+return Logger
